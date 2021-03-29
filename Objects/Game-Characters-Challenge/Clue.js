7555697c9e80c8f3fbe9ebf56be38797346365a4
ManuelG28 <manuelalejandrogutierrezmejia@gmail.com>
2021-03-29 11:22:12 -0500

    Adding an explanation

--- Objects/Game-Characters-Challenge/Clue.js
+++ Objects/Game-Characters-Challenge/Clue.js
@@ -1,3 +1,8 @@
+/**
+ * A simple representation to the board game called 'Clue'
+ * With this challenge I've understood the difference between Arrays and Objects
+ * and the big difference between brackets notation and dot notation
+ */
 const suspects = ["Mrs.White", "Mrs.Green","Colonel Mustard","Miss Scarlet","Mrs.Peacock","Mrs.Green"]
 
 const rooms = ["Kitchen","Ballroom","Conservatory","Dining Room","Loungue","Hall","Study","Library","Conservatory"
